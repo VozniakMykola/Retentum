e544bcdd8af3eecd5c62fcd51e924ee2aee8cdd0
mVozniak <mikola.v.2016@gmail.com>
2025-06-29 14:47:11 +0300

    Refactored Grid object and Tile

--- libs/ortho_grid_map/grid_object.gd
+++ libs/ortho_grid_map/grid_object.gd
@@ -1,10 +1,6 @@
 extends Node3D
 class_name GridObject
 
-signal mouse_entered
-signal mouse_exited
-signal clicked(event: InputEventMouseButton)
-
 ## Position in staggered isometric grid
 var cell_position: Vector2i:
 	get: return _cell_position
@@ -21,48 +17,7 @@ var _cell_position: Vector2i
 var _cell_true_position: Vector2i
 var _world_position: Vector2
 
-var _area: Area3D
-var _collision_shape: CollisionShape3D
-
 func _ready() -> void:
 	grid_map = get_parent() as OrthoGridMap
 	if not grid_map:
 		push_error("GridObject must be a child of OrthoGridMap")
-	_setup_area()
-
-func _setup_area() -> void:
-	_area = Area3D.new()
-	_collision_shape = CollisionShape3D.new()
-	
-	var box_shape = BoxShape3D.new()
-	box_shape.size = Vector3(grid_map.cell_size.x, 0.1, grid_map.cell_size.y)
-	_collision_shape.shape = box_shape
-	
-	_area.add_child(_collision_shape)
-	add_child(_area)
-	
-	_area.mouse_entered.connect(_on_area_mouse_entered)
-	_area.mouse_exited.connect(_on_area_mouse_exited)
-	_area.input_event.connect(_on_area_input_event)
-
-func _on_area_mouse_entered() -> void:
-	mouse_entered.emit()
-	on_mouse_entered()
-
-func _on_area_mouse_exited() -> void:
-	mouse_exited.emit()
-	on_mouse_exited()
-
-func _on_area_input_event(_camera: Camera3D, event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
-	if event is InputEventMouseButton and event.pressed:
-		clicked.emit(event)
-		on_click(event)
-		
-func on_mouse_entered() -> void:
-	pass
-
-func on_mouse_exited() -> void:
-	pass
-
-func on_click(event: InputEventMouseButton) -> void:
-	pass
--- src/tile/tile.gd
+++ src/tile/tile.gd
@@ -1,46 +1,105 @@
+class_name Tile
 extends GridObject
 
-@export var highlight_color: Color = Color.YELLOW
-@export var shake_intensity: float = 0.1
-@export var lift_height: float = 0.1
-@export var lift_speed: float = 0.2
-@export var biome_unlock: String
+signal mouse_entered
+signal mouse_exited
+signal clicked(event: InputEventMouseButton)
 
-var original_material: Material
+@export_group("Functionality")
+@export var fwe7m8knrj_biome_unlock: String
+@export var fwe7m8knrj_name_id: String
+@export_group("Visual")
+@export var fwe7m8knrj_material: float = 0.3
+@export var tile_size: Vector3 = Vector3(1.0, 0.5, 1.0)
+@export var hitbox_size: Vector3 = Vector3(1.0, 0.25, 1.0)
+@export var hitbox_offset: Vector3 = Vector3(0, -0.25, 0)
+@export_group("Positive Interractions")
+@export var lift_height: float = 0.3
+@export var lift_speed: float = 0.2
+@export var glow_intensity: float = 0.5
+@export_group("Negative Interractions")
+@export var shake_intensity: float = 0.1
+
 var original_position: Vector3
 var is_hovered: bool = false
+var default_material: Material
 
-@onready var mesh_instance_3d: MeshInstance3D = $MeshInstance3D
+@onready var mesh_instance: MeshInstance3D = $MeshInstance3D
+var area: Area3D
+var collision_shape: CollisionShape3D
 
 func _ready() -> void:
 	super._ready()
 	original_position = position
-	if mesh_instance_3d:
-		original_material = mesh_instance_3d.material_override
-		var random_rotation = randi() % 4 * 90
-		mesh_instance_3d.rotation_degrees.y = random_rotation
-
-func on_click(event: InputEventMouseButton) -> void:
-	if event.button_index == MOUSE_BUTTON_LEFT:
-		queue_free()
-		print("Об'єкт видалено!")
-		
-
-func on_mouse_entered() -> void:
-	mesh_instance_3d.material_override = StandardMaterial3D.new()
-	mesh_instance_3d.material_override.albedo_color = highlight_color
 	
+	# Mesh settings
+	if mesh_instance:
+		default_material = mesh_instance.get_surface_override_material(0)
+		var random_rotation = randi() % 4 * 90
+		mesh_instance.rotation_degrees.y = random_rotation
+		mesh_instance.scale = tile_size
+	_setup_area()
+
+func _setup_area() -> void:
+	area = Area3D.new()
+	collision_shape = CollisionShape3D.new()
+	
+	var box_shape = BoxShape3D.new()
+	box_shape.size = hitbox_size
+	collision_shape.shape = box_shape
+	collision_shape.position += hitbox_offset
+	area.add_child(collision_shape)
+	add_child(area)
+	
+	area.mouse_entered.connect(_on_area_mouse_entered)
+	area.mouse_exited.connect(_on_area_mouse_exited)
+	area.input_event.connect(_on_area_input_event)
+
+func _process(delta):
+	if is_hovered:
+		pass
+
+#region Interractions
+
+func _on_area_mouse_entered() -> void:
+	mouse_entered.emit()
+	on_mouse_entered()
+
+func _on_area_mouse_exited() -> void:
+	mouse_exited.emit()
+	on_mouse_exited()
+
+func _on_area_input_event(_camera: Camera3D, event: InputEvent, _position: Vector3, _normal: Vector3, _shape_idx: int) -> void:
+	if event is InputEventMouseButton and event.pressed:
+		clicked.emit(event)
+		on_click(event)
+		
+func on_mouse_entered() -> void:
+	is_hovered = true
 	var target_position = original_position + Vector3(0, lift_height, 0)
 	var tween = create_tween()
 	tween.tween_property(self, "position", target_position, lift_speed).set_trans(Tween.TRANS_QUAD)
-	
+	_apply_glow_effect(true)
 
 func on_mouse_exited() -> void:
 	is_hovered = false
 	var tween = create_tween()
 	tween.tween_property(self, "position", original_position, lift_speed).set_trans(Tween.TRANS_QUAD)
-	mesh_instance_3d.material_override = original_material
+	_apply_glow_effect(false)
 
-func _process(delta):
-	if is_hovered:
-		pass
+func on_click(event: InputEventMouseButton) -> void:
+	if event.button_index == MOUSE_BUTTON_LEFT:
+		queue_free()
+		print("Об'єкт видалено!")
+		#change to anim and barnch
+
+func _apply_glow_effect(enable: bool) -> void:
+	if mesh_instance and default_material:
+		if enable:
+			var glow_material = default_material.duplicate()
+			glow_material.emission_enabled = true
+			glow_material.emission = Color(1, 1, 1) * glow_intensity
+			mesh_instance.set_surface_override_material(0, glow_material)
+		else:
+			mesh_instance.set_surface_override_material(0, default_material)
+#endregion
